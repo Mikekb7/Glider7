486412109f182b7b5e68c0ca0970f4ce11425a47
mekht <mekhtnh@gmail.com>
2024-12-06 11:43:07 -0500

    first 2

--- src/main/java/org/example/glider7/test.java
+++ src/main/java/org/example/glider7/test.java
@@ -2,6 +2,6 @@ package org.example.glider7;
 
 public class test {
     public static void main(String[] args){
-        System.out.println("Java pt 1");
+        System.out.println("Java pt 1 3");
     }
 }
